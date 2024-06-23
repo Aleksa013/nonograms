a4f1c93bf465e010f03b85a3ccc7b3bc88d6c76e
Aleksa013 <velma86@bk.ru>
2024-06-23 17:24:43 +0300

    feat: delete unused files

--- src/functions/getNewGame.js
+++
@@ -1,9 +0,0 @@
-import {ctx, drawField, getPicture} from '../components/field';
-
-
-const getNewGame = (data, number) => { 
-  ctx.clearRect(0, 0, 500, 500);
-  getPicture(data).then(drawField(data[number], data[number]));
-}
-
-export {getNewGame}
